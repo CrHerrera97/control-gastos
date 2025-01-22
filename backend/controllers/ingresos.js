f179ac5dd20886553b176f2348ef38079673f68d
christian herrera <cristian.2221@hotmail.es>
2025-01-22 17:01:35 -0500

    ajuste en el filtro de ingresos

--- backend/controllers/ingresos.js
+++ backend/controllers/ingresos.js
@@ -4,11 +4,24 @@ const { response } = require('express');
 const Ingreso = require('../models/ingreso')
 
 const obtenerIngresos = async (req,res = response) => {
-
-    const ingresos = await Ingreso.find().populate({
+    
+    const {limite = '5',desde = '0'} = req.query
+    /*
+    const ingresos = await Ingreso.find({ estado: true }).populate({
         path: 'categoria',
         select: '-descripcion -estado -creadoEn -__v'
     })
+
+    */
+   const [ ingresos ] = await Promise.all([
+    Ingreso.find({ estado: true })
+    .populate({
+        path: 'categoria',
+        select: '-descripcion -estado -creadoEn -__v'
+    })
+    .skip(desde)
+    .limit(limite)
+   ])
 
     res.status(200).json({
         ingresos
@@ -81,10 +94,11 @@ const editarIngreso = async (req,res) => {
 
     const categoria = req.body.categoria;
     const valor = req.body.valor;
+    const estado = req.body.estado;
 
     const actualizarIngreso = await Ingreso.findByIdAndUpdate(
         id,
-        { $set: { categoria, valor }}
+        { $set: { categoria, valor, estado }}
     )
 
     res.status(200).json(actualizarIngreso)
