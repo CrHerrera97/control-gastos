2cf5a8e2d0fe37939c3dd5c9d600f46710757357
christian herrera <cristian.2221@hotmail.es>
2025-01-28 08:03:14 -0500

    fixing in gastos form

--- frontend/src/components/Gastos/GastoForm.jsx
+++ frontend/src/components/Gastos/GastoForm.jsx
@@ -4,127 +4,99 @@ import { Table, Spinner, Modal, Form, Button, ListGroup } from 'react-bootstrap'
 const GastosForm = ({ showModal, handleClose, tipoModal, currentGasto, setCurrentGasto, handleSaveChanges, handleCategoriaChange, filteredCategoriasGasto, handleCategoriaGastoSelect }) => {
     return (
       <div>
-        {/* Modal Ingresar/Editar */}
+      {/* Modal Ingresar/Editar */}
         <Modal show={showModal} onHide={handleClose}>
-        <Modal.Header closeButton>
+          <Modal.Header closeButton>
           <Modal.Title>{tipoModal}</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
+          </Modal.Header>
+          <Modal.Body>
           {currentGasto !== null && (
-          <Form>
+            <Form>
             <Form.Group controlId="id">
-              <Form.Label>Id Gasto</Form.Label>
-              <Form.Control type="text" value={''} readOnly/>
+          <Form.Label>Id Gasto</Form.Label>
+          <Form.Control type="text" value={currentGasto.id || ''} readOnly />
+            </Form.Group>
+            <Form.Group controlId="idCategoria">
+          <Form.Label>Id Categoría</Form.Label>
+          <Form.Control
+            type="text"
+            value={currentGasto?.categoria?._id || ''}
+            readOnly
+          />
             </Form.Group>
             <Form.Group controlId="categoria">
-              <Form.Label>Id Categoria</Form.Label>
-              <Form.Control
-                type="text"
-                value={currentGasto.categoria || ''}
-                autocomplete="off"
-                onChange={(e) => setCurrentGasto({ ...currentGasto, categoria: e.target.value })}
-              />
-            <Form.Label>Categoría</Form.Label>
-            <Form.Control
-              type="text"
-              value={currentGasto?.categoria?.nombre || ''}
-              autocomplete="off"
-              onChange={handleCategoriaChange}
-              placeholder="Escribe para buscar categorías..."
+          <Form.Label>Categoría</Form.Label>
+          <Form.Control
+          type="text"
+          value={currentGasto?.categoria?.nombre || ''}
+          autoComplete="off"
+          onChange={(e) => {
+              handleCategoriaChange(e);
+              setCurrentGasto({ ...currentGasto, categoria: { nombre: e.target.value } });
+            }}
+            placeholder="Escribe para buscar categorías..."
             />
-            { filteredCategoriasGasto.length > 0 && (
-              <ListGroup>
+            {filteredCategoriasGasto.length > 0 && (
+            <ListGroup>
               {filteredCategoriasGasto.map((categoria) => (
-                <ListGroup.Item
+              <ListGroup.Item
                 key={categoria._id}
-                //action
-                onClick={() => handleCategoriaGastoSelect(categoria)}
-                >
+                onClick={() => {
+                handleCategoriaGastoSelect(categoria);
+                setCurrentGasto({ ...currentGasto, categoria: categoria });
+                handleCategoriaChange({ target: { value: '' } }); // Clear the list
+                }}
+              >
                 {categoria.nombre}
-                </ListGroup.Item>
+              </ListGroup.Item>
               ))}
-              </ListGroup>
+            </ListGroup>
             )}
-            </Form.Group>
-            {/* Sub categorias*/}
-            <Form.Group controlId="subCategoria">
+          </Form.Group>
+          <Form.Group controlId="subCategoria">
             <Form.Label>Id subCategoria</Form.Label>
             <Form.Control
-              type="text"
-              value={currentGasto.subCategoria || ''} 
-              onChange={(e) => setCurrentGasto({ ...currentGasto, subCategoria: e.target.value })}
+            type="text"
+            value={currentGasto.subCategoria || ''} 
+            onChange={(e) => setCurrentGasto({ ...currentGasto, subCategoria: e.target.value })}
             />
             <Form.Label>Sub Categoría</Form.Label>
             <Form.Control
-              type="text"
-              value={''}
-              //onChange={(e) => setCurrentGasto({ ...currentGasto, subCategoria: e.target.value })}
-              placeholder="Escribe para buscar Subcategorías..."
+            type="text"
+            value={''}
+            placeholder="Escribe para buscar Subcategorías..."
             />
-            {/*showSuggestions && filteredCategorias.length > 0 && (
-              <ListGroup>
-              {filteredCategorias.map((categoria) => (
-                <ListGroup.Item
-                key={categoria._id}
-                action
-                onClick={() => handleCategoriaSelect(categoria)}
-                >
-                {categoria.nombre}
-                </ListGroup.Item>
-              ))}
-              </ListGroup>
-            )*/}
-            </Form.Group>
-            {/* Descripcion */}
-            <Form.Group controlId="descripcion">
+          </Form.Group>
+          {/* Descripcion */}
+          <Form.Group controlId="descripcion">
             <Form.Label>Descripcion</Form.Label>
             <Form.Control
-              type="text"
-              autocomplete="off"
-              value={currentGasto.descripcion || ''} 
-              onChange={(e) => setCurrentGasto({ ...currentGasto, descripcion: e.target.value })}
-              //onChange={(e) => setCurrentIngreso({ ...currentGasto, valor: e.target.value })}
+            type="text"
+            autoComplete="off"
+            value={currentGasto.descripcion || ''} 
+            onChange={(e) => setCurrentGasto({ ...currentGasto, descripcion: e.target.value })}
             />
-            </Form.Group>
-            <Form.Group controlId="valor">
+          </Form.Group>
+          <Form.Group controlId="valor">
             <Form.Label>Valor</Form.Label>
             <Form.Control
-              type="number"
-              value={currentGasto.valor || ''}
-              onChange={(e) => setCurrentGasto({ ...currentGasto, valor: e.target.value })}
+            type="number"
+            value={currentGasto.valor || ''}
+            onChange={(e) => setCurrentGasto({ ...currentGasto, valor: e.target.value })}
             />
-            </Form.Group>
+          </Form.Group>
           </Form>
-          )}
+        )}
         </Modal.Body>
         <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
+        <Button variant="secondary" onClick={handleClose}>
           Cerrar
-          </Button>
-          <Button variant="primary" onClick={()=>handleSaveChanges(currentGasto)}>
+        </Button>
+        <Button variant="primary" onClick={() => handleSaveChanges(currentGasto)}>
           Guardar Cambios
-          </Button>
+        </Button>
         </Modal.Footer>
-        </Modal>
-        {/* Modal Eliminar */}
-        {/*
-        <Modal show={modalEliminar} onHide={handleCloseDelete}>
-              <Modal.Header closeButton>
-                <Modal.Title>{tipo}</Modal.Title>
-              </Modal.Header>
-              <Modal.Body>
-                ¿Estás seguro de que deseas eliminar este registro de ingreso?
-              </Modal.Body>
-              <Modal.Footer>
-                <Button variant="secondary" onClick={handleCloseDelete}>
-                  Cancelar
-                </Button>
-                <Button variant="danger" onClick={()=>handleDelete(currentGasto)}>
-                  Eliminar
-                </Button>
-              </Modal.Footer>
-            </Modal>
-          */}
+      </Modal>
       </div>
     )
 }
