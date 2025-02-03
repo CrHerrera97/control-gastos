15d08b87ca1acf9be7f6e8860d5e743a154d27f9
christian herrera <cristian.2221@hotmail.es>
2025-02-03 09:21:05 -0500

    pequeño ajuste en dashboard

--- frontend/src/components/DashBoard.jsx
+++ frontend/src/components/DashBoard.jsx
@@ -1,20 +1,42 @@
 import React, { useState, useEffect } from 'react';
 
-const valorTotalIngresos = () =>{
+const dashboard = () => {
 
     const url = import.meta.env.VITE_API_URL;
     const port = import.meta.env.VITE_PORT;
 
-    const [ total, setTotal] = useState(1);
-    // Llamado a el fetch
-    fetch(`${url}:${port}/api/ingresos/total/amount`)
-    .then((response)=> response.json())
-    .then((total) => {
-        setTotal(total.valorTotal)
-    })
+    const [total, setTotal] = useState(1);
+    const [saldo, setSaldo] = useState(1);
+
+    useEffect(() => {
+        const valorTotal = () => {
+            // Llamado a el fetch
+            fetch(`${url}:${port}/api/ingresos/total/amount`)
+            .then((response)=> response.json())
+            .then((total) => {
+                setTotal(total.valorTotal)
+            })
+        }
+
+        const saldoActual = () => {
+            // Llamado a el fetch
+            fetch(`${url}:${port}/api/gastos/saldo/amount`)
+            .then((response)=> response.json())
+            .then((saldo) => {
+                setSaldo(saldo.valorTotal)
+            })
+        }
+
+        valorTotal();
+        saldoActual();
+    }, []);
+
     return (
-        <div>Valor total Ingresos: {total}</div>
+        <div>
+            <div>Valor total Ingresos: {total}</div>
+            <div>Saldo: {saldo}</div>
+        </div>
     )
 }
 
-export default valorTotalIngresos
+export default dashboard
